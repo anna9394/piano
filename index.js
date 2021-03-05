dcf7b08496563a9c8f3d9f1b45298818f422d218
Hanna Osipenko <annaandreevna9394@gmail.com>
2021-03-05 17:33:46 +0300

    feat: add piano functionality

--- index.js
+++ index.js
@@ -4,60 +4,92 @@ const audio = document.querySelector('audio');
 const button = document.querySelector('button');
 const notes = document.querySelector('.notes');
 const letters = document.querySelector('.letters');
+const fullScreen = document.querySelector('.fullScreen');
+let toggle = false;
 
 
-// прогрывание музыки
-piano.addEventListener('click', (event) => {
-    if(event.target.classList.contains('piano-key')) {
-        const note = event.target.dataset.note;
-        const src = `assets/audio/${note}.mp3`;
-        playAudio(src);
-      }     
-  });
 
-
-// подсветка активной клавиши | не работает на черных!?? 
-  piano.addEventListener('click', (event) => {
-    if(event.target.classList.contains('piano-key')) {
-      pianoКeys.forEach((el) => {
-        if(el.classList.contains('active')) {
-          el.classList.remove('active');
-          console.log(el)
-        }
-      });
-      event.target.classList.add('active');
-    }
-});
-
-function playAudio() {
-  audio.currentTime = 0;
-  audio.play();
+function playAudio(event) {
+  const audio = document.querySelector(`audio[data-note="${event.type === 'mousedown' || event.type === 'mouseover' ? event.target.dataset.note : event.keyCode}"]`);
+  if (audio) {
+    audio.play(event);
+  }
 }
 
-// проигрывание музыки при  нажатии на клавиатуру
+// проигрывание музыки при  нажатии на клавиатуру и добавлении класса активности
   window.addEventListener('keydown', (event) => {
-    // switch(event.code){
-       
-    //     case 'KeyC':
-    //     case 'KeyD':
-    //     case 'KeyE':
-    //     case 'KeyF':
-    //     case 'KeyG':
-    //     case 'KeyA':
-    //     case 'KeyB':
-            playAudio()  
-    // }
-    });
-
-   
-// игра через пробел
-window.addEventListener('keydown', (event) => {
-    if(event.code === 'Space') {
-      playAudio();
+    const pressedButton = document.querySelector(`.piano-key[data-note="${event.keyCode}"]`);
+    if (pressedButton) {
+      pressedButton.classList.add('active')
+      playAudio(event)
     }
   });
 
-//   button.addEventListener('click', playAudio)
+  window.addEventListener('keyup', (event) => {
+    const pressedButton = document.querySelector(`.piano-key[data-note="${event.keyCode}"]`);
+    if (pressedButton) {
+      pressedButton.classList.remove('active')
+    }
+  });
+
+
+
+// проигрывание музыки при нажатии на мышь и добавлении класса активности
+
+window.addEventListener('mousedown', (event) => {
+  toggle = true
+  const pressedButton = document.querySelector(`.piano-key[data-note="${event.target.dataset.note}"]`);
+
+  if (pressedButton) {
+    pressedButton.classList.add('active')
+    audio.currentTime = 0
+    playAudio(event)
+  }
+});
+
+window.addEventListener('mouseup', (event) => {
+  toggle = false
+  const pressedButton = document.querySelector(`.piano-key[data-note="${event.target.dataset.note}"]`);
+  if (pressedButton) {
+    pressedButton.classList.remove('active')
+  }
+});
+
+
+// проигрывание музыки при зажатой мышке и добавлении класса активности
+
+window.addEventListener('mouseover', (event) => {
+  const pressedButton = document.querySelector(`.piano-key[data-note="${event.target.dataset.note}"]`);
+  if (pressedButton && toggle) {
+    pressedButton.classList.add('active')
+    audio.currentTime = 0
+    playAudio(event)
+  }
+});
+
+window.addEventListener('mouseout', (event) => {
+  const pressedButton = document.querySelector(`.piano-key[data-note="${event.target.dataset.note}"]`);
+  if (pressedButton) {
+    pressedButton.classList.remove('active')
+  }
+});
+
+//отключение контекст-меню
+window.addEventListener('contextmenu', e => e.preventDefault())
+
+// full-screen
+fullScreen.addEventListener("click",function(){
+  if(!window.screenTop && !window.screenY){
+    document.documentElement.requestFullscreen();
+    fullScreen.classList.add('exit')
+  } else{
+    fullScreen.classList.remove('exit')
+    document.exitFullscreen();
+  }
+});
+
+
+// переключатель нот/ букв
 
 
 
